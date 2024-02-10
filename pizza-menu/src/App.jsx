56f615feed9090abf4b46669444a4c57bcec8e29
ceddevstudent89 <cedstudent89@gmail.com>
2024-02-10 22:55:16 +0100

    Ajout du style

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -1,4 +1,5 @@
 import { createElement } from "react";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -47,7 +48,7 @@ const pizzaData = [
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -56,18 +57,26 @@ export default function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style} className="header">
+        Fast React Pizza Co.
+      </h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 
@@ -77,8 +86,8 @@ function Footer() {
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
-  if (isOpen) alert("We're currently open !");
-  else alert("Sorry we're closed");
+  // if (isOpen) alert("We're currently open !");
+  // else alert("Sorry we're closed");
 
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
@@ -87,10 +96,10 @@ function Footer() {
 
 function Pizza() {
   return (
-    <div>
+    <footer className="footer">
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
+    </footer>
   );
 }
