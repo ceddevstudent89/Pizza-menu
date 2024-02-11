5098f81ce6ce33eaf1fa2e4497bce7970e7b00dc
ceddevstudent89 <cedstudent89@gmail.com>
2024-02-11 12:12:45 +0100

    Ajout props dans le composant Pizza

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -72,11 +72,34 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}€</span>
+      </div>
+    </div>
   );
 }
 
@@ -91,15 +114,5 @@ function Footer() {
 
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <footer className="footer">
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </footer>
   );
 }
